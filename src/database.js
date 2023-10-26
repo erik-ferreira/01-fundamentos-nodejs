a6533fcd2c7314ba61a44c165219c5f63f5e4351
erik-ferreira <erikdesouzagf@gmail.com>
2023-10-25 21:31:54 -0300

    add update user; extract query param to filter users; complete project

--- src/database.js
+++ src/database.js
@@ -19,8 +19,16 @@ export class Database {
     fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
 
-  select(table) {
-    const data = this.#database[table] ?? []
+  select(table, search) {
+    let data = this.#database[table] ?? []
+
+    if (search) {
+      data = data.filter((row) => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
 
     return data
   }
@@ -42,7 +50,16 @@ export class Database {
 
     if (rowIndex > -1) {
       this.#database[table].splice(rowIndex, 1)
-      this.#persist
+      this.#persist()
+    }
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data }
+      this.#persist()
     }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,16 @@ export const routes = [
     method: "GET",
     path: buildRoutePath("/users"),
     handler: (req, res) => {
-      const users = database.select("users")
+      const { search } = req.query
+
+      const searchInUsers = search
+        ? {
+            name: search,
+            email: search,
+          }
+        : null
+
+      const users = database.select("users", searchInUsers)
 
       return res.end(JSON.stringify(users))
     },
@@ -39,6 +48,18 @@ export const routes = [
       const { id } = req.params
 
       database.delete("users", id)
+
+      return res.writeHead(204).end()
+    },
+  },
+  {
+    method: "PUT",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+
+      database.update("users", id, { name, email })
 
       return res.writeHead(204).end()
     },
